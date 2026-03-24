1c3616a7a2d9da424b797904ab37feef9b9604da
vrynkov@yahoo.com <vrynkov@yahoo.com>
2026-03-24 17:04:25 -0400

    server.py modified - path to html needs to be specified

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request 
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("EmotionDetection")
+app = Flask(__name__, template_folder='templates')
 
-@app.route('emotionDetector')
-def home():
+@app.route("/") 
+def render_index_page(): 
     return render_template('index.html')
 
+
+@app.route('/emotionDetector')
+def detect_emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    return response
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
