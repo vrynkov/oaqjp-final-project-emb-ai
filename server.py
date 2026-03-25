a295355862f22818c99f7e55dc567d2ec0faf35e
vrynkov@yahoo.com <vrynkov@yahoo.com>
2026-03-24 21:40:17 -0400

    added coments to server.py to make it compliant with pylint

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template, request 
+'''
+server.py
+
+Calls utility for getting predominant mood from the user input.
+'''
+
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route("/") 
-def render_index_page(): 
+@app.route("/")
+def render_index_page():
+    ''' present user with the index.html where user provides their input '''
     return render_template('index.html')
 
 
 @app.route('/emotionDetector')
 def detect_emotion():
+    ''' extract user input and uses it to determine users emotion '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-
     return response
 
 
